206dd877cabab731ba9c257c2eb6a6b4eba0153b
Jake Noel-Storr <noelstoj@gmail.com>
2023-04-25 04:01:10 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'BlaauwObserverKnoweldgebase'
+copyright = '2023, University of Groningen'
+author = 'Jake Noel-Storr'
 
 release = '0.1'
 version = '0.1.0'
