bb4688495afa9591b47d74be2562e5db89aa6cd5
Sten Sipma <sten.sipma@ziggo.nl>
2023-12-02 11:20:54 +0100

    Add sphinx rtd theme to extensions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,44 +2,45 @@
 
 # -- Project information
 
-project = 'BlaauwObserverKnoweldgebase'
-copyright = '2023, University of Groningen'
-author = 'Jake Noel-Storr'
+project = "BlaauwObserverKnoweldgebase"
+copyright = "2023, University of Groningen"
+author = "Jake Noel-Storr"
 
-release = '0.1'
-version = '0.1.0'
+release = "0.1"
+version = "0.1.0"
 
 # -- General configuration
 
 extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
+    "sphinx.ext.duration",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
+    "sphinx_rtd_theme",
 ]
 
 intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+    "python": ("https://docs.python.org/3/", None),
+    "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
 }
-intersphinx_disabled_domains = ['std']
+intersphinx_disabled_domains = ["std"]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # -- Options for HTML output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # -- Options for EPUB output
-epub_show_urls = 'footnote'
+epub_show_urls = "footnote"
 
-extensions.append('sphinx.ext.todo')
-todo_include_todos=True
+extensions.append("sphinx.ext.todo")
+todo_include_todos = True
 
 # -- Custom style file
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 html_css_files = [
-    'style.css',
+    "style.css",
 ]
